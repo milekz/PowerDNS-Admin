9f8d0b037e8012e1ea86eb680d8f80120b4cef30
root <root@ip-172-31-16-249.eu-west-2.compute.internal>
2018-12-06 22:25:04 +0000

    posfres

---
+++ db_schema/15-import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp -f /docker-entrypoint-initdb.d/20-schema.xxx /tmp/20-schema.yyy
+
+sed -i "s/PGDB_USER/$PGSQL_USER/g" /tmp/20-schema.yyy
+sed -i "s/PGDB_PASSWORD/$PGSQL_PASS/g" /tmp/20-schema.yyy
+sed -i "s/PGDB_NAME/$PGSQL_DBNAME/g" /tmp/20-schema.yyy
+
+psql  < /tmp/20-schema.yyy
+
+psql -U ${PGSQL_USER} ${PGSQL_DBNAME} < /docker-entrypoint-initdb.d/30-schema.xxx
+
+unset PGSQL_PASS
+rm /tmp/20-schema.yyy
+ 
