fc09e2af10df0d444a28292927eb4a31f0f84905
root <root@ip-172-31-16-249.eu-west-2.compute.internal>
2018-12-08 11:38:36 +0000

    clen up

--- db_schema/15-import.sh
+++ db_schema/15-import.sh
@@ -13,6 +13,7 @@ sed -i "s/PDA_PASSWORD/$PDA_PGDB_PASS/g" /tmp/20-schema.yyy
 sed -i "s/PDA_NAME/$PDA_PGDB_DBNAME/g" /tmp/20-schema.yyy
 
 sed -i "s/PDNS_PGDB_REPLICA_PASSWORD/$PDNS_PGDB_REPLICA_PASSWORD/g" /tmp/20-schema.yyy
+sed -i "s/PDNS_PGDB_REPLICA_USER/$PDNS_PGDB_REPLICA_USER/g" /tmp/20-schema.yyy
 
 
 sed -i "s/PDNS_PGDB_REPLICA_USER/$PDNS_PGDB_REPLICA_USER/g" /tmp/30-schema.yyy
@@ -24,16 +25,10 @@ psql  < /tmp/20-schema.yyy
 
 psql -U ${PGSQL_USER} ${PGSQL_DBNAME} < /tmp/30-schema.yyy
 
-#unset PGSQL_PASS
-#export PGSQL_PASS=xxxxxxxxxxxxxxxxxxx
-
-#rm /tmp/20-schema.yyy
-
-
-#echo "wal_level = replica" >> ${PGDATA}/postgresql.conf 
-#echo "max_wal_senders = 3" >> ${PGDATA}/postgresql.conf 
-#echo "wal_keep_segments = 64" >> ${PGDATA}/postgresql.conf 
 
 echo "wal_level = logical" >> ${PGDATA}/postgresql.conf 
 
 echo "host     replication     $PDNS_PGDB_REPLICA_USER       all      md5" >> ${PGDATA}/pg_hba.conf
+
+rm -rf  /tmp/20-schema.yyy
+rm -rf  /tmp/30-schema.yyy
