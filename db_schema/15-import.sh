2ea5055d32d97fa4da05d4c115b5dcd48f02d751
root <root@ip-172-31-16-249.eu-west-2.compute.internal>
2018-12-08 11:13:53 +0000

    postgresupdate & logical replication

--- db_schema/15-import.sh
+++ db_schema/15-import.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -f /docker-entrypoint-initdb.d/20-schema.xxx /tmp/20-schema.yyy
+cp -f /docker-entrypoint-initdb.d/30-schema.xxx /tmp/30-schema.yyy
 
 sed -i "s/PGDB_USER/$PGSQL_USER/g" /tmp/20-schema.yyy
 sed -i "s/PGDB_PASSWORD/$PGSQL_PASS/g" /tmp/20-schema.yyy
@@ -14,11 +15,14 @@ sed -i "s/PDA_NAME/$PDA_PGDB_DBNAME/g" /tmp/20-schema.yyy
 sed -i "s/PDNS_PGDB_REPLICA_PASSWORD/$PDNS_PGDB_REPLICA_PASSWORD/g" /tmp/20-schema.yyy
 
 
+sed -i "s/PDNS_PGDB_REPLICA_USER/$PDNS_PGDB_REPLICA_USER/g" /tmp/30-schema.yyy
+
+
 
 
 psql  < /tmp/20-schema.yyy
 
-psql -U ${PGSQL_USER} ${PGSQL_DBNAME} < /docker-entrypoint-initdb.d/30-schema.xxx
+psql -U ${PGSQL_USER} ${PGSQL_DBNAME} < /tmp/30-schema.yyy
 
 #unset PGSQL_PASS
 #export PGSQL_PASS=xxxxxxxxxxxxxxxxxxx
@@ -26,7 +30,10 @@ psql -U ${PGSQL_USER} ${PGSQL_DBNAME} < /docker-entrypoint-initdb.d/30-schema.xx
 #rm /tmp/20-schema.yyy
 
 
-echo "wal_level = replica" >> ${PGDATA}/postgresql.conf 
-echo "max_wal_senders = 3" >> ${PGDATA}/postgresql.conf 
-echo "wal_keep_segments = 64" >> ${PGDATA}/postgresql.conf 
- 
+#echo "wal_level = replica" >> ${PGDATA}/postgresql.conf 
+#echo "max_wal_senders = 3" >> ${PGDATA}/postgresql.conf 
+#echo "wal_keep_segments = 64" >> ${PGDATA}/postgresql.conf 
+
+echo "wal_level = logical" >> ${PGDATA}/postgresql.conf 
+
+echo "host     replication     $PDNS_PGDB_REPLICA_USER       all      md5" >> ${PGDATA}/pg_hba.conf
